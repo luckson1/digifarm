63b9f08de439d5b38268e2183b72e89380733dbe
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-07-18 00:41:50 +0300

    fix:debug small screen css

--- src/app/dashboard/components/dashboard.tsx
+++ src/app/dashboard/components/dashboard.tsx
@@ -27,14 +27,14 @@ export default function DashboardPage() {
   }, [auth, router, setForm]);
   return (
     <>
-      <div className="hidden flex-col md:flex">
+      <div className="flex-col flex">
         <div className="border-b">
           <div className="flex h-16 items-center px-4">
             <div className="ml-auto flex items-center space-x-4"></div>
           </div>
         </div>
         <div className="flex-1 space-y-4 p-8 pt-6">
-          <div className="flex items-center justify-between space-y-2">
+          <div className="flex flex-col md:flex-row items-center justify-between md:space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
             <div className="flex items-center space-x-2">
               <CalendarDateRangePicker />
